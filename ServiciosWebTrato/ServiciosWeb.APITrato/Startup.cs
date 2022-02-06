6a90876d5c2ca126eb5815195937ac0a957dd98d
Royer Alfonso Hernandez Morales <royer.hernandez@servientrega.com>
2022-02-05 20:57:08 -0500

    Add Newtonsoft.Json.ReferenceLoopHandling.Ignore for problems with ReferenceLoopHandling

--- ServiciosWebTrato/ServiciosWeb.APITrato/Startup.cs
+++ ServiciosWebTrato/ServiciosWeb.APITrato/Startup.cs
@@ -29,7 +29,9 @@ namespace ServiciosWeb.APITrato
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(options => {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
 
             services.AddDbContext<TRATO_DATABASEContext>( options =>
                                      options.UseSqlServer(Configuration.GetConnectionString("TratoDB")));
